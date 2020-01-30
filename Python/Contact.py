82e4799b3ee74a342fe62bed3b28543e2ec43d19
goe152 <goe152@naver.com>
2020-01-30 17:39:24 +0900

    [add]python_study

--- Python/Contact.py
+++ Python/Contact.py
@@ -3,22 +3,25 @@ def private_info(**kwargs):
     # print(kwargs)
     return kwargs
 
+print(num)
 
-num = int(input("번호를 입력하세요."))
+num = contact_program(num)
 def contact_program(num):
 
     while num != 5:
-
+        num = int(input("번호를 입력하세요."))
         if num == 1:
 
             name = str(input("이름을 입력하세요."))
             phone_num = str(input("전화번호를 입력하세요."))
             gubun = str(input("관계를 입력하세요"))
-            private_info(name=name,phone_num=phone_num,gubun=gubun)
+            print(private_info(name=name,phone_num=phone_num,gubun=gubun))
             # private_info('이름'=name,'전화번호'=phone_num, "구분"=gubun)
-            print(private_info)
+            # prin
+        elif num == 2:
 
-    return num
-num = contact_program(num)
+
+
+    # return num
 # print( num = int(input("번호를 입력하세요")))
-print(num)
+# print(num)
---
+++ Python/test4.py
@@ -0,0 +1,17 @@
+# def print_kwargs(**kwargs):
+#     # print(kwargs)
+
+#     return kwargs
+
+# print(print_kwargs(a=1,b=2,c=2))
+
+a = str(input("1. "))
+b = str(input("2. "))
+c = str(input("3. "))
+    
+
+def print_kwargs(**kwargs):
+    
+    return kwargs
+
+print(print_kwargs(name=a,cl=b,tes=c))
