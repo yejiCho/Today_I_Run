85cd3b1fe50c60679205ce96f9243ef20cfe0c13
goe152 <goe152@naver.com>
2020-02-05 16:57:32 +0900

    revise dir

--- Python/2020_02_03.py
+++ Python/2020_02_03.py
@@ -15,20 +15,45 @@
 # print(cal2.add(3))
 # print(cal2.add(7))
 
-result1 = 0
-result2 = 0
+# result1 = 0
+# result2 = 0
 
-def add1(num):
-    global result1
-    result1 += num
-    return result1
+# def add1(num):
+#     global result1
+#     result1 += num
+#     return result1
 
-def add2(num):
-    global result2
-    result2 += num
-    return result2
+# def add2(num):
+#     global result2
+#     result2 += num
+#     return result2
 
-print(add1(3))
-print(add1(4))
-print(add2(3))
-print(add2(7))
+# print(add1(3))
+# print(add1(4))
+# print(add2(3))
+# print(add2(7))
+
+class Calculator:
+    def __init__(self):
+        self.result = 0
+    
+    def add(self,num):
+        self.result += num
+        return self.result
+
+
+
+class Calculators:
+    def __init__(self):
+        self.result = 0
+    
+    def mul(self,num):
+        self.result -= num
+        return self.result
+
+if __name__=="__main__":
+    cal1 = Calculator()
+    cal2 = Calculator()
+    cal3 = Calculators()
+    print(cal1(3))
+    print(cal1(5))
